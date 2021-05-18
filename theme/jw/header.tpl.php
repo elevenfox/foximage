2c5dd2a184a1771509be88601182d2be4700f57d
Eric Hu <eric@sagegraph.com>
2021-05-18 05:05:39 +0000

    change site name

--- theme/jw/header.tpl.php
+++ theme/jw/header.tpl.php
@@ -87,7 +87,7 @@ $domainUrl = getDomainUrl();
             </div>
             <a href="/" title="Home" rel="home" class="header__logo">
                 <img src="/theme/jw/logo.png" alt="Home" class="header__logo-image">
-                <span>图城<span class="not-in-mobile">- 高清美女图集</span></span>
+                <span>图宅<span class="not-in-mobile">- 高清美女图集</span></span>
             </a>
             <div id="search-button-div">
                 <a href="#" id="buttonSearch" class="ui-link"><img src="/theme/jw/images/search100bsa.png" title="Search" alt="Search"></a>

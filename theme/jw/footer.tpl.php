3f3b6c798d2fc44d5c81aaeb0f2eed870bcc78cf
Eric Hu <eric@sagegraph.com>
2021-07-09 20:27:22 +0000

    change site name

--- theme/jw/footer.tpl.php
+++ theme/jw/footer.tpl.php
@@ -70,7 +70,7 @@
            <span class="footer-link"><a href="/privacy-policy">Privacy Policy</a></span>
        </div>
        <div style="margin: 15px 0;">
-           © JAVWall8 图城 2020 - <?=date('Y', time())?>
+           © Tuzac 图宅网 2020 - <?=date('Y', time())?>
        </div>
        <?php print $data['scripts_footer']; ?>
 

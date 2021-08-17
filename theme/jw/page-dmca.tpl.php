f12b897ed2b20fae4fbdb08e3927cd39e801cd2a
Eric Hu <eric@sagegraph.com>
2021-08-17 23:39:38 +0000

    remove javwall8 text in several places

--- theme/jw/page-dmca.tpl.php
+++ theme/jw/page-dmca.tpl.php
@@ -18,22 +18,22 @@ include 'header.tpl.php';
         <div id="content" class="content">
 
             <div class="text-container">
-                <p>www.JAVWall8.com (“JAVWall”) qualifies as a “Service Provider” within the meaning of 17 U.S.C. § 512(k)(1) of the Digital Millennium Copyright Act (“DMCA”). &nbsp;</p>
+                <p>www.tuzac.com (TUZAC) qualifies as a “Service Provider” within the meaning of 17 U.S.C. § 512(k)(1) of the Digital Millennium Copyright Act (“DMCA”). &nbsp;</p>
 
-                <p>Accordingly, it is entitled to certain protections from claims of copyright infringement under the DMCA, commonly referred to as the “safe harbor” provisions. We respect the intellectual property of others, and we ask our users to do the same. We will respond to notices of alleged copyright infringement that comply with the “DMCA” or any other applicable intellectual property legislation or laws. Responses may include removing, blocking or disabling access to material claimed to be the subject of infringing activity, terminating the user’s access to JAVWall, or all of the foregoing.</p>
+                <p>Accordingly, it is entitled to certain protections from claims of copyright infringement under the DMCA, commonly referred to as the “safe harbor” provisions. We respect the intellectual property of others, and we ask our users to do the same. We will respond to notices of alleged copyright infringement that comply with the “DMCA” or any other applicable intellectual property legislation or laws. Responses may include removing, blocking or disabling access to material claimed to be the subject of infringing activity, terminating the user’s access to TUZAC, or all of the foregoing.</p>
 
                 <p>Accordingly, we observe and comply with the DMCA, and have adopted the following Notice and Takedown Policy relating to claims of copyright infringement by our customers, subscribers or users.</p>
 
                 <p><br>
                     <strong>REPORTING CLAIMS OF COPYRIGHT INFRINGEMENT</strong></p>
 
-                <p>If you believe any material accessible on JAVWall infringes your copyright, you may submit a copyright infringement notification. These requests should only be submitted by the copyright owner or an agent authorized to act on the owner’s behalf with the following information:</p>
+                <p>If you believe any material accessible on TUZAC infringes your copyright, you may submit a copyright infringement notification. These requests should only be submitted by the copyright owner or an agent authorized to act on the owner’s behalf with the following information:</p>
 
                 <p>(a) an electronic or physical signature of the person authorized to act on behalf of the owner of the copyright or other intellectual property interest;</p>
 
                 <p>(b) description of the copyrighted work or other intellectual property that you claim has been infringed;</p>
 
-                <p>(c) a description of where the material that you claim is infringing is located on JAVWall (preferably including specific url’s associated with the material);</p>
+                <p>(c) a description of where the material that you claim is infringing is located on TUZAC (preferably including specific url’s associated with the material);</p>
 
                 <p>(d) your address, telephone number, and email address;</p>
 
@@ -47,20 +47,20 @@ include 'header.tpl.php';
 
                 <p>If you fail to comply with all of the requirements of Section 512(c)(3) of the DMCA, your DMCA Notice may not be effective.</p>
 
-                <p>Please be aware that if you knowingly materially misrepresent that material or activity on JAVWall is infringing your copyright, you may be held liable for damages (including costs and attorneys’ fees) under Section 512(f) of the DMCA.</p>
+                <p>Please be aware that if you knowingly materially misrepresent that material or activity on TUZAC is infringing your copyright, you may be held liable for damages (including costs and attorneys’ fees) under Section 512(f) of the DMCA.</p>
 
-                <p>The copyright owner’s name will be published on JAVWall in place of disabled content. This will become part of the public record of your DMCA Notice, along with your description of the work(s) allegedly infringed. All the information provided in a DMCA Notice, the actual DMCA Notice (including your personal information), or both may be forwarded to the uploader of the allegedly infringing content. By submitting a DMCA Notice, you consent to having your information revealed in this way.</p>
+                <p>The copyright owner’s name will be published on TUZAC in place of disabled content. This will become part of the public record of your DMCA Notice, along with your description of the work(s) allegedly infringed. All the information provided in a DMCA Notice, the actual DMCA Notice (including your personal information), or both may be forwarded to the uploader of the allegedly infringing content. By submitting a DMCA Notice, you consent to having your information revealed in this way.</p>
 
                 <p><br>
                     <strong>DMCA COUNTER-NOTIFICATION PROCEDURES</strong></p>
 
-                <p>If you have received a DMCA Notice and believe that material you posted on JAVWall was removed or access to it was disabled by mistake or misidentification, you are permitted to submit a counter-notification pursuant to Section 512(g)(2)&amp;(3) of the DMCA. Counter notifications must be submitted by the File’s original uploader or an agent authorized to act on their behalf. The information that a Recipient provides in a counter-notification must be accurate and truthful, and the Recipient will be liable for any misrepresentations which may cause any claims to be brought against JAVWall relating to the actions taken in response to the counter-notification.</p>
+                <p>If you have received a DMCA Notice and believe that material you posted on TUZAC was removed or access to it was disabled by mistake or misidentification, you are permitted to submit a counter-notification pursuant to Section 512(g)(2)&amp;(3) of the DMCA. Counter notifications must be submitted by the File’s original uploader or an agent authorized to act on their behalf. The information that a Recipient provides in a counter-notification must be accurate and truthful, and the Recipient will be liable for any misrepresentations which may cause any claims to be brought against TUZAC relating to the actions taken in response to the counter-notification.</p>
 
                 <p>Pursuant to the DMCA, the Counter-Notice must include substantially the following information:</p>
 
                 <p>(a) a specific description of the material that was removed or disabled pursuant to the Notice;</p>
 
-                <p>(b) a description of where the material was located within JAVWall or the Content before such material was removed and/or disabled (preferably including specific url’s associated with the material);</p>
+                <p>(b) a description of where the material was located within TUZAC or the Content before such material was removed and/or disabled (preferably including specific url’s associated with the material);</p>
 
                 <p>(c) a statement reflecting the Recipient's belief that the removal or disabling of the material was done so erroneously. &nbsp;For convenience, the following format may be used:</p>
 
@@ -82,24 +82,24 @@ include 'header.tpl.php';
 
                 <p><strong>REPEAT INFRINGERS</strong></p>
 
-                <p>In accordance with the DMCA and other applicable law, we have adopted a policy of terminating or disabling, in appropriate circumstances and at our sole discretion, the accounts of users who are deemed to be repeat infringers. We may also at our sole discretion limit access to JAVWall, terminate or disable the accounts of any users who infringe any intellectual property rights of others, whether or not there is any repeat infringement.</p>
+                <p>In accordance with the DMCA and other applicable law, we have adopted a policy of terminating or disabling, in appropriate circumstances and at our sole discretion, the accounts of users who are deemed to be repeat infringers. We may also at our sole discretion limit access to TUZAC, terminate or disable the accounts of any users who infringe any intellectual property rights of others, whether or not there is any repeat infringement.</p>
 
                 <p><br>
                     <strong>TAKE DOWN PROCEDURE</strong></p>
 
-                <p>JAVWall implements the following “notification and takedown” procedure upon receipt of any notification of claimed copyright infringement. &nbsp;JAVWall reserves the right at any time to disable access to, or remove any material or activity accessible on or from JAVWall or any Materials claimed to be infringing or based on facts or circumstances from which infringing activity is apparent. It is the firm policy of JAVWall to terminate the account of repeat copyright infringers, when appropriate, and JAVWall will act expeditiously to remove access to all material that infringes on another’s copyright, according to the procedure set forth in 17 U.S.C. §512 of the Digital Millennium Copyright Act (“DMCA”). JAVWall DMCA Notice Procedures are set forth in the preceding paragraph. If the notice does not comply with §512 of the DMCA, but does comply with three requirements for identifying sites that are infringing according to §512 of the DMCA, JAVWall shall attempt to contact or take other reasonable steps to contact the complaining party to help that party comply with the notice requirements. When we receive a valid notice, JAVWall will expeditiously remove and/or disable access to the infringing material and shall notify the affected user. Then, the affected user may submit a counter-notification to us containing a statement made under penalty of perjury that the user has a good faith belief that the material was removed because of misidentification of the material. After we receive the counter-notification, it will replace the material at issue within 10-14 days after receipt of the counter-notification unless we receive notice that a court action has been filed by the complaining party seeking an injunction against the infringing activity. JAVWall reserves the right to modify, alter or add to this policy, and all users should regularly check back to these Terms and Conditions to stay current on any such changes.</p>
+                <p>TUZAC implements the following “notification and takedown” procedure upon receipt of any notification of claimed copyright infringement. &nbsp;TUZAC reserves the right at any time to disable access to, or remove any material or activity accessible on or from TUZAC or any Materials claimed to be infringing or based on facts or circumstances from which infringing activity is apparent. It is the firm policy of TUZAC to terminate the account of repeat copyright infringers, when appropriate, and TUZAC will act expeditiously to remove access to all material that infringes on another’s copyright, according to the procedure set forth in 17 U.S.C. §512 of the Digital Millennium Copyright Act (“DMCA”). TUZAC DMCA Notice Procedures are set forth in the preceding paragraph. If the notice does not comply with §512 of the DMCA, but does comply with three requirements for identifying sites that are infringing according to §512 of the DMCA, TUZAC shall attempt to contact or take other reasonable steps to contact the complaining party to help that party comply with the notice requirements. When we receive a valid notice, TUZAC will expeditiously remove and/or disable access to the infringing material and shall notify the affected user. Then, the affected user may submit a counter-notification to us containing a statement made under penalty of perjury that the user has a good faith belief that the material was removed because of misidentification of the material. After we receive the counter-notification, it will replace the material at issue within 10-14 days after receipt of the counter-notification unless we receive notice that a court action has been filed by the complaining party seeking an injunction against the infringing activity. TUZAC reserves the right to modify, alter or add to this policy, and all users should regularly check back to these Terms and Conditions to stay current on any such changes.</p>
 
                 <p><br>
                     <strong>MODIFICATIONS TO POLICY</strong></p>
 
-                <p>JAVWall reserves the right to modify, alter or add to this policy, and all affected persons should regularly check back regularly to stay current on any such changes.</p>
+                <p>TUZAC reserves the right to modify, alter or add to this policy, and all affected persons should regularly check back regularly to stay current on any such changes.</p>
 
                 <p>&nbsp;</p>
 
                 <p><br>
                     <strong>CUSTOMER SERVICE REQUESTS</strong></p>
 
-                <p>Please note that the DMCA Agent is not associated with the Site in any other capacity, but is an attorney with a private law firm. Customer service inquiries, payment questions, and cancellation requests will not receive a response. All such communications must be directed to the Site's customer service department. service@JAVWall8.com</p>
+                <p>Please note that the DMCA Agent is not associated with the Site in any other capacity, but is an attorney with a private law firm. Customer service inquiries, payment questions, and cancellation requests will not receive a response. All such communications must be directed to the Site's customer service department. service@tuzac.com</p>
 
                 <p>&nbsp;</p>
             </div>
--- theme/jw/page-usc-2257.tpl.php
+++ theme/jw/page-usc-2257.tpl.php
@@ -18,21 +18,21 @@ include 'header.tpl.php';
         <div id="content" class="content">
 
             <div class="text-container">
-                <p>JAVWall8.com is not a producer (primary or secondary) of any and all of the content found on the website (JAVWall8.com). With respect to the records as per 18 USC 2257 for any and all content found on this site, please kindly direct your request to the site for which the content was produced.</p>
+                <p>Tuzac.com is not a producer (primary or secondary) of any and all of the content found on the website (tuzac.com). With respect to the records as per 18 USC 2257 for any and all content found on this site, please kindly direct your request to the site for which the content was produced.</p>
 
-                <p>JAVWall8.com is a File sharing site in which allows for the uploading, sharing and general viewing of various types of adult content and while Pornhub.com does the best it can with verifying compliance, it may not be 100% accurate.</p>
+                <p>Tuzac.com is a File sharing site in which allows for the uploading, sharing and general viewing of various types of adult content and while Pornhub.com does the best it can with verifying compliance, it may not be 100% accurate.</p>
 
-                <p>JAVWall8.com abides by the following procedures to ensure compliance:</p>
+                <p>Tuzac.com abides by the following procedures to ensure compliance:</p>
 
                 <p>Requiring all users to be 18 years of age to upload Files.</p>
 
                 <p>When uploading, user must verify the content; assure he/she is 18 years of age; certify that he/she keeps records of the models in the content and that they are over 18 years of age.</p>
 
-                <p>For further assistance and/or information in finding the content's originating site, please contact JAVWall8.com compliance at service@JAVWall8.com</p>
+                <p>For further assistance and/or information in finding the content's originating site, please contact tuzac.com compliance at service@tuzac.com</p>
 
-                <p>JAVWall8.com allows content to be flagged as inappropriate. Should any content be flagged as illegal, unlawful, harassing, harmful, offensive or various other reasons, JAVWall8.com shall remove it from the site without delay.</p>
+                <p>Tuzac.com allows content to be flagged as inappropriate. Should any content be flagged as illegal, unlawful, harassing, harmful, offensive or various other reasons, tuzac.com shall remove it from the site without delay.</p>
 
-                <p>Users of JAVWall8.com who come across such content are urged to flag it as inappropriate by emailing service@JAVWall8.com.&nbsp;</p>
+                <p>Users of tuzac.com who come across such content are urged to flag it as inappropriate by emailing service@tuzac.com.&nbsp;</p>
             </div>
 
         </div>

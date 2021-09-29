454a99fbd7ebf539072f52e94ee6a8d2ead9aaa8
Eric Hu <eric@sagegraph.com>
2021-09-29 18:21:04 +0000

    use juicyadds google-friendly popup for kkc3

--- theme/kkc3/header.tpl.php
+++ theme/kkc3/header.tpl.php
@@ -82,9 +82,12 @@ $domainUrl = getDomainUrl();
 <?php
 if( empty($data['dev_mode']) ) {
 ?>
-<!-- JuicyAds PopUnders v3 Start -->
+<!-- JuicyAds v3.2P Start -->
+<script type="text/javascript">
+var juicy_tags = ['a', 'img'];
+</script>
 <script type="text/javascript" src="https://js.juicyads.com/jp.php?c=34b42303x224u4q2v284039484&u=http%3A%2F%2Fwww.juicyads.rocks"></script>
-<!-- JuicyAds PopUnders v3 End -->
+<!-- JuicyAds v3.2P End -->
 
 <!-- never block -->
 <script type="text/javascript" src="/nb/sdBZfpSvXN.js"></script>

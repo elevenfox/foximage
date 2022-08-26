e76a00e24b35506cf9b1a24552cd0625cac0a446
Eric Hu <eric@sagegraph.com>
2022-08-26 00:49:26 +0000

    more scripts

---
+++ misc/z_import_physical.php
@@ -0,0 +1,108 @@
+<?php
+require_once '../bootstrap.inc';
+
+set_time_limit(0);
+ini_set('memory_limit','2048M');
+ini_set('display_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+
+###################### Define variables ################
+
+$file_root = Config::get('file_root');
+
+$folder_full_path = isset($argv[1]) ? $argv[1] : null;
+if(empty($folder_full_path)) {
+    echo "---- Must have a full folder path as the param! \n";
+    exit;
+}
+###################### End of define variables ################
+
+
+###################### Define functions ################
+function file_db_exist($relative_path) {
+	$res = File::getFileBySourceUrl($relative_path);
+
+	if(!$res) {
+		$res = File::getIgnoredFileBySourceUrl($relative_path);
+	}
+
+	return $res;
+}
+###################### end of define functions ################
+
+echo  "#####################################\n";
+echo  date('Y-m-d H:i:s') . ' - ' . "Start importing albums .... \n";
+echo  date('Y-m-d H:i:s') . ' - ' . "-- on " . Config::get('theme') . " \n";
+echo  date('Y-m-d H:i:s') . ' - ' . "-- for folder: " . $folder_full_path . " \n";
+echo  "#####################################\n";
+
+// Start importing to current (dev) site: build fileObj from physical files
+echo date('Y-m-d H:i:s') . ' - ' . "-- importing: $folder_full_path \n";
+
+$relative_path = str_replace($file_root, '', $folder_full_path);
+
+$fileObj = new stdClass();
+$fileObj->source = 'manual';
+$fileObj->source_url = $relative_path;
+
+// title
+$fileObj->title = basename($folder_full_path);
+
+// description
+$fileObj->description = file_get_contents($folder_full_path.'/desc.txt');
+
+// tags
+$fileObj->tags = file_get_contents($folder_full_path.'/tags.txt');
+
+// images
+$files = scandir($folder_full_path);
+$num_images = 0;
+$phy_images = [];
+foreach ($files as $f) {
+    if($f != '.' && $f != '..' && $f != 'thumbnail.jpg') {
+        $extension = pathinfo($physical_path. '/' .$f, PATHINFO_EXTENSION);
+        if($extension == 'jpg') {
+            $phy_images[] = $f;
+            $num_images++;
+        }
+    }
+}
+
+echo date('Y-m-d H:i:s') . '----- physical image count = ' . $num_images . "\n";
+
+// Rename all images by 000 -> 999 number file name with a postfix to avoid override
+$i = 1;
+$new_file_name_arr = [];
+$postfix = time();
+foreach ($phy_images as $f) {
+    $new_file_name = sprintf('%03d',$i) . '-'.$postfix.'.jpg';
+    echo date('Y-m-d H:i:s') .  '----- change '.$f." to ".$new_file_name;
+    $res = rename($physical_path.'/'.$f, $physical_path.'/'.$new_file_name);
+    echo $res ? " 1 \n" : " 0 \n";
+    $new_file_name_arr[] = $new_file_name;
+    $i++;
+}
+// remove postfix
+$i = 1;
+$new_file_name_arr2 = [];
+foreach ($new_file_name_arr as $nf) {
+    $new_file_name = sprintf('%03d',$i) . '.jpg';
+    echo date('Y-m-d H:i:s') .  '----- change '.$nf." to ".$new_file_name;
+    $res = rename($physical_path.'/'.$nf, $physical_path.'/'.$new_file_name);
+    echo $res ? " 1 \n" : " 0 \n";
+    $new_file_name_arr2[] = $new_file_name;
+    $i++;
+}
+
+$fileObj->images = implode(',', $new_file_name_arr2);
+$fileObj->thumbnail = 1;
+
+File::save($fileObj);
+
+$fileRowDB = file_db_exist($relative_path);
+if($fileRowDB) {
+	// Update the row in the database
+	echo date('Y-m-d H:i:s') . "----- Update db row ...... \n";
+	$query = 'update '. $pre . 'files set full_path=2, saved_locally=5  where id='.$fileRowDB['id'];
+	DB::$dbInstance->getRows($query);
+}
---
+++ misc/z_reorg_file_by_path.php
@@ -0,0 +1,62 @@
+<?php
+require_once '../bootstrap.inc';
+
+set_time_limit(0);
+ini_set('memory_limit','2048M');
+ini_set('display_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+
+$prod_api = Config::get('prod_api_url');
+
+$folder_full_path = isset($argv[1]) ? (int)$argv[1] : null;
+if(empty($folder_full_path)) {
+    echo "---- Must have a full folder path as the param! \n";
+    exit;
+}
+
+echo "#####################################\n";
+echo date('Y-m-d H:i:s') . ' - ' . "Start loop photos .... \n";
+echo date('Y-m-d H:i:s') . ' - ' . "-- in folder: " . $folder_full_path . " \n";
+echo "#####################################\n";
+
+// Get physical file cound, 
+// exclude ., .., thumbnail.jpg, non-jpg files
+$physical_path = $folder_full_path;
+$files = scandir($physical_path);
+$num_images = 0;
+$phy_images = [];
+foreach ($files as $f) {
+    if($f != '.' && $f != '..' && $f != 'thumbnail.jpg') {
+        $extension = pathinfo($physical_path. '/' .$f, PATHINFO_EXTENSION);
+        if($extension == 'jpg') {
+            $phy_images[] = $f;
+            $num_images++;
+        }
+    }
+}
+
+echo date('Y-m-d H:i:s') . '----- physical image count = ' . $num_images . "\n";
+
+// Rename all images by 000 -> 999 number file name with a postfix to avoid override
+$i = 1;
+$new_file_name_arr = [];
+$postfix = time();
+foreach ($phy_images as $f) {
+    $new_file_name = sprintf('%03d',$i) . '-'.$postfix.'.jpg';
+    echo date('Y-m-d H:i:s') .  '----- change '.$f." to ".$new_file_name;
+    $res = rename($physical_path.'/'.$f, $physical_path.'/'.$new_file_name);
+    echo $res ? " 1 \n" : " 0 \n";
+    $new_file_name_arr[] = $new_file_name;
+    $i++;
+}
+// remove postfix
+$i = 1;
+$new_file_name_arr2 = [];
+foreach ($new_file_name_arr as $nf) {
+    $new_file_name = sprintf('%03d',$i) . '.jpg';
+    echo date('Y-m-d H:i:s') .  '----- change '.$nf." to ".$new_file_name;
+    $res = rename($physical_path.'/'.$nf, $physical_path.'/'.$new_file_name);
+    echo $res ? " 1 \n" : " 0 \n";
+    $new_file_name_arr2[] = $new_file_name;
+    $i++;
+}

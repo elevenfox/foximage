31b4f02803d57c85904648a023697a15405b4128
Eric Hu <eric@sagegraph.com>
2022-08-30 00:33:13 +0000

    resize image shellscript

---
+++ misc/image_resize_wm.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# change directory to the desired one
+cd $1
+echo $1
+echo ''
+
+# look up files with extensions
+for fname in *.*; do
+  if [[ $(file -b ${fname}) =~ ^'JPEG ' ]]; then
+    # get the width of the image
+    WIDTH=`identify -ping -format '%w' ${fname}`
+    # get the height of the image
+    HEIGHT=`identify -ping -format '%h' ${fname}`
+    
+    MAX_SIZE=$WIDTH
+    if [ $WIDTH -lt $HEIGHT ]; then
+        MAX_SIZE=$HEIGHT
+    fi
+    echo "-- image width: $WIDTH, height: ${HEIGHT}...max_size: ${MAX_SIZE}"
+    
+    if [ $MAX_SIZE -gt 2400 ]; then
+        PERCENTAGE=$((200*2400/$MAX_SIZE % 2 + 100*2400/$MAX_SIZE))
+        TO_PERC=$((100-$PERCENTAGE))
+
+        # resize and set quality to 80% to make smaller file
+        echo "resizing $fname by $TO_PERC% ..."
+        mogrify -resize ${TO_PERC}%  -quality 80 "$fname"
+    fi
+
+    # this is the watermarking part
+    if [ $MAX_SIZE -gt 1024 ]; then
+        
+        POINT=20
+        if [ $MAX_SIZE -ge 1800 ]; then
+            POINT=30
+        fi
+        echo "watermarking $fname with pointsize ${POINT}"
+        convert $fname -font Liberation-Sans-Bold -pointsize $POINT -draw "gravity southeast fill black text 0,12 'TUZAC.com  ' fill white text 1,11 'TUZAC.com  '" $fname
+    fi
+  fi
+done
+
+echo ''

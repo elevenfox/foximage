06508c565ed89f2057ae768eb451095b3ff88473
Eric Hu <eric@sagegraph.com>
2022-09-01 19:29:14 +0000

    rar-tuzac command

--- misc/format_folder_name.sh
+++ misc/format_folder_name.sh
@@ -5,8 +5,12 @@ cur_folder=$1
 
 echo "Current folder is: ${cur_folder}"
 
+
+#######################################
+# Define functions
+#######################################
 format_folder_name () {
-  old_name=$1;
+  old_name=$1
   new_name=${old_name//\'/-}
   new_name=${new_name//\"/-}
   new_name=${new_name// - /-}
@@ -14,29 +18,73 @@ format_folder_name () {
   new_name=${new_name//(/-}
   new_name=${new_name//) /-}
   new_name=${new_name//)/-}
+  new_name=${new_name//  /-}
   new_name=${new_name// /-}
+  new_name=${new_name//--/-}
 
   echo ${new_name}
 }
 
+reorg_files () {
+  full_dir_path=$1
+  
+  # loop folder, exclude ., .., thumbnail.jpg, non-jpg files
+  echo "--- reorg files in folder: ${full_dir_path}"
+  cd "${full_dir_path}"
+
+  # rename files to 000-->999 with postfix to avoid override
+  i=1
+  timestamp=$(date +%s)
+  filelist=$(ls | grep -i '.jpg')
+  for f in $filelist; do
+    if [ $f != "thumbnail.jpg" ]; then
+      num=`printf "%03d" ${i}`
+      echo "---- mv ${f} --> ${num}-${timestamp}.jpg"
+      mv "${f}" ${num}-${timestamp}.jpg
+      i=$((i+1))
+    fi  
+  done
+
+  # remove postfix
+  i=1
+  filelist=$(ls | grep -i '.jpg')
+  for f in $filelist; do
+    if [ $f != "thumbnail.jpg" ]; then
+      num=`printf "%03d" ${i}`
+      echo "---- mv ${f} --> ${num}.jpg"
+      mv "${f}" ${num}.jpg
+      i=$((i+1))
+    fi  
+  done
+}
+#######################################
+# End of functions
+#######################################
+
 new_cur_folder_name=`format_folder_name "${cur_folder}"`
 
 if [ "${new_cur_folder_name}" != "${cur_folder}" ]; then
-  echo " ----> ${new_cur_folder_name}"
+  echo " --> ${new_cur_folder_name}"
   mv "${cur_folder}" "${new_cur_folder_name}"
   echo ''
 fi
 
 cd "${new_cur_folder_name}"
+reorg_files "${new_cur_folder_name}"
 
-# look up files with extensions
+# look up sub folders
 for d in */; do
-  new_d=`format_folder_name "${d}"`
-  if [ "${new_d}" != "${d}" ]; then
-    echo "---- ${d}"
-    echo "  ----> ${new_d}"
-    mv "${d}" "${new_d}"
-  fi
+  if [ $d != "*/" ]; then
+    new_d=`format_folder_name "${d}"`
+    if [ "${new_d}" != "${d}" ]; then
+      echo "-- ${d}"
+      echo "  --> ${new_d}"
+      mv "${d}" "${new_d}"
+    fi
+    # re org file names to 000 --> 999 format
+    reorg_files "${new_d}"
+    cd ..
+  fi  
 done
 
 echo ''

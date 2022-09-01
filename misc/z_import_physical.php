5378ad600a5e1794796eb85b61f019770b90ebaa
Eric Hu <eric@sagegraph.com>
2022-09-01 04:01:17 +0000

    match JPG extension too when importing

--- misc/z_import_physical.php
+++ misc/z_import_physical.php
@@ -77,7 +77,7 @@ $phy_images = [];
 foreach ($files as $f) {
     if($f != '.' && $f != '..' && $f != 'thumbnail.jpg' && substr($f, 0, 1) != ".") {
         $extension = pathinfo($physical_path. '/' .$f, PATHINFO_EXTENSION);
-        if($extension == 'jpg') {
+        if($extension == 'jpg' || $extension == 'JPG') {
             $phy_images[] = $f;
             $num_images++;
         }

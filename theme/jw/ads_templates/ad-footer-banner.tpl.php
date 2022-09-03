11c7d325dd8a27612e379d28d77c202f16a2b132
Eric Hu <eric@sagegraph.com>
2022-09-03 17:00:50 +0000

    change to adult ads to see if can get higher cpm

--- theme/jw/ads_templates/ad-footer-banner.tpl.php
+++ theme/jw/ads_templates/ad-footer-banner.tpl.php
@@ -4,20 +4,14 @@ if( empty($data['dev_mode']) ) {
 
 <div id="block-ads-jp-footer-banner-ad">
     <div class="ad-container">
-        <script type="application/javascript">
-          var ad_idzone = "4279902",
-            ad_width = "728",
-            ad_height = "90"
-        </script>
-        <script type="application/javascript" src="https://a.realsrv.com/ads.js"></script>
-        <noscript>
-            <iframe src="https://syndication.realsrv.com/ads-iframe-display.php?idzone=4279902&output=noscript&type=728x90" width="728" height="90" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
-        </noscript>
+      <script async type="application/javascript" src="https://a.realsrv.com/ad-provider.js"></script> 
+      <ins class="adsbyexoclick" data-zoneid="4779886"></ins> 
+      <script>(AdProvider = window.AdProvider || []).push({"serve": {}});</script>
     </div>
     <script type="text/javascript">
       (function () {
         function randStr(e,t){for(var n="",r=t||"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",o=0;o<e;o++)n+=r.charAt(Math.floor(Math.random()*r.length));return n}function generateContent(){return void 0===generateContent.val&&(generateContent.val=" \ndocument.dispatchEvent("+randStr(4*Math.random()+3)+");"),generateContent.val}try{Object.defineProperty(document.currentScript,"innerHTML",{get:generateContent}),Object.defineProperty(document.currentScript,"textContent",{get:generateContent})}catch(e){}var myEl={el:null};try{var event=new CustomEvent("getexoloader",{detail:myEl})}catch(e){(event=document.createEvent("CustomEvent")).initCustomEvent("getexoloader",!1,!1,myEl)}window.document.dispatchEvent(event);var ExoLoader=myEl.el;
-        ExoLoader.addZone({"idzone":"4279902"}); 
+        ExoLoader.addZone({"idzone":"4779886"}); 
       })();
     </script>
 </div>
--- theme/jw/ads_templates/ad-side-right.tpl.php
+++ theme/jw/ads_templates/ad-side-right.tpl.php
@@ -15,14 +15,14 @@
     <div id="block-ads-right-sidebar-ad-v">
         <div>
             <div class="ad-container">
-              <ins class="adsbyexoclick" data-zoneid="4279904"></ins> 
+              <ins class="adsbyexoclick" data-zoneid="4779888"></ins> 
             </div>
         </div>
     </div>
     <div id="block-ads-right-sidebar-ad-v-2">
         <div>
             <div class="ad-container">
-              <ins class="adsbyexoclick" data-zoneid="4279906"></ins> 
+              <ins class="adsbyexoclick" data-zoneid="4779890"></ins> 
             </div>
         </div>
     </div>
@@ -30,8 +30,8 @@
     <script type="text/javascript">
               (function () {
                 function randStr(e,t){for(var n="",r=t||"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",o=0;o<e;o++)n+=r.charAt(Math.floor(Math.random()*r.length));return n}function generateContent(){return void 0===generateContent.val&&(generateContent.val=" \ndocument.dispatchEvent("+randStr(4*Math.random()+3)+");"),generateContent.val}try{Object.defineProperty(document.currentScript,"innerHTML",{get:generateContent}),Object.defineProperty(document.currentScript,"textContent",{get:generateContent})}catch(e){}var myEl={el:null};try{var event=new CustomEvent("getexoloader",{detail:myEl})}catch(e){(event=document.createEvent("CustomEvent")).initCustomEvent("getexoloader",!1,!1,myEl)}window.document.dispatchEvent(event);var ExoLoader=myEl.el;
-                ExoLoader.addZone({"idzone":"4279904"}); // native-v
-                ExoLoader.addZone({"idzone":"4279906"}); // native-v2
+                ExoLoader.addZone({"idzone":"4779888"}); // native-v
+                ExoLoader.addZone({"idzone":"4779890"}); // native-v2
               })();
     </script>
 </div>
